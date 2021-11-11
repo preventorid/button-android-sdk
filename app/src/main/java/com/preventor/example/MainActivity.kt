295eb2c4997446bb9c5cc5740d261793751bfd37
cesarrobledorivera <45716528+robledoce@users.noreply.github.com>
2021-11-11 10:03:26 -0500

    implementation example

--- app/src/main/java/com/preventor/example/MainActivity.kt
+++ app/src/main/java/com/preventor/example/MainActivity.kt
@@ -7,5 +7,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val preventorSDK = PreventorSDK(this, this)
+        val identityVerificationButton = findViewById<PreventorButton>(R.id.identityVerificationButton)
+
+        val config = preventorSDK.getConfig()
+
+        config.setFlowType("YOUR_FLOW_TYPE");
+        config.getCredentials().setApiKey("YOUR_API_KEY");
+        config.getCredentials().setClientSecret("YOUR_CLIENT_SECRET");
+        config.getCredentials().setTenant("YOUR_TENANT");
+        config.getCredentials().setBanknu("YOUR_BANKNU");
+        config.getCredentials().setEnv("YOUR_ENV");
+
+        config.getCurrentUserInfo().setCifCode("YOUR_CIFCODE");
+
+
+        config.getTheme().setLogo(R.drawable.your_logo);  //YOUR_LOGO
+        config.getTheme().getButton().setBackgroundColor("YOUR_BACKGROUND_COLOR");
+        config.getTheme().getButton().getFont().setFontFamily("YOUR_FONT_FAMILY");
+        config.getTheme().getButton().getFont().setFontSize(14); //"YOUR_FONT_SIZE"
+        config.getTheme().getButton().getFont().setFontColor("YOUR_FONT_COLOR");
+
+        preventorSDK.setConfig(config)
+
+        identityVerificationButton.apply()
+
+        identityVerificationButton.setOnClickListener {
+            preventorSDK.validateApiKey()
+        }
+
+        preventorSDK.callback(object : PreventorSDKListener {
+            override fun onStart() {
+                println("MainActivity onStart");
+            }
+            override fun onFinish() {
+                println("MainActivity onFinish");
+            }
+            override fun onError(error: String) {
+                println("MainActivity onError: $error");
+            }
+            override fun onSubmitted() {
+                println("MainActivity onSubmitted");
+            }
+        })
     }
 }
