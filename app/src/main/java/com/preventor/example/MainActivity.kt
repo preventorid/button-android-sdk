cb1e276e4b406580a8b5010e416c7b6d2a0ba7e6
Cesar Robledo Rivera <robledoce@gmail.com>
2021-12-15 14:54:42 -0500

    Preventor ID  project

--- app/src/main/java/com/preventor/example/MainActivity.kt
+++ app/src/main/java/com/preventor/example/MainActivity.kt
@@ -2,7 +2,6 @@ package com.preventor.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.util.*
 import com.preventor.pvtidentityverification.PreventorSDK
 import com.preventor.pvtidentityverification.PreventorSDKListener
 import com.preventor.pvtidentityverification.widgets.PreventorButton
@@ -15,25 +14,32 @@ class MainActivity : AppCompatActivity() {
         val preventorSDK = PreventorSDK(this, this)
         val identityVerificationButton = findViewById<PreventorButton>(R.id.identityVerificationButton)
 
+
         val config = preventorSDK.getConfig()
 
+
         config.flowType = "YOUR_FLOW_TYPE"
+
         config.credentials.apiKey = "YOUR_API_KEY"
         config.credentials.clientSecret = "YOUR_CLIENT_SECRET"
         config.credentials.tenant = "YOUR_TENANT"
         config.credentials.banknu = "YOUR_BANKNU"
         config.credentials.env = "YOUR_ENV"
 
-        config.currentUserInfo.cifCode = UUID.randomUUID().toString()
+        config.currentUserInfo.cifCode = "YOUR_CIFCODE"
 
+        // 4. SET THE THEME
 
-        config.theme.logo = R.drawable.ic_logotipo
-        config.theme.button.backgroundColor = "YOUR_BACKGROUND_COLOR"
-        config.theme.button.font.fontFamily = "YOUR_FONT_FAMILY"
+        config.theme.logo = R.drawable.your_logo //YOUR_LOGO
+
+        config.theme.button.backgroundColor = "#FFFFFF" //YOUR_BACKGROUND_COLOR
         config.theme.button.font.fontSize = 14 //"YOUR_FONT_SIZE"
-        config.theme.button.font.fontColor = "YOUR_FONT_COLOR"
+        config.theme.button.font.fontColor = "#1B2358" //YOUR_FONT_COLOR
+        config.theme.steps.font.fontFamily = preventorSDK.tahoma_regular //YOUR_FONT_SELECTED
 
-        preventorSDK.setConfig(config)
+
+
+        config.theme.steps.primaryColor = preventorSDK.preventor_theme //YOUR_THEME_COLOR_SELECTED
 
         identityVerificationButton.apply()
 
